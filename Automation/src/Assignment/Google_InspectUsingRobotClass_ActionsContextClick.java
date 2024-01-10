7c63ec25b77ceb4ed9d8bbf939a2bbceee7c4cf9
HP <HP@HP-Astha>
2024-01-10 19:39:43 +0530

    KeyBoard Handling

---
+++ Automation/src/Assignment/Google_InspectUsingRobotClass_ActionsContextClick.java
@@ -0,0 +1,53 @@
+package Assignment;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class Google_InspectUsingRobotClass_ActionsContextClick {
+	
+	public static void main(String[] args) throws AWTException {
+		
+		WebDriver d = new ChromeDriver();
+		
+		d.manage().window().maximize();
+		
+		d.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		
+		d.get("https://www.google.com/");
+		
+		// create Object for Actions Class & contextClick (right click option)
+		Actions act = new Actions(d);
+		act.contextClick().perform();
+		
+		// create Object for Robot Class
+		Robot r = new Robot();
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+		r.keyPress(KeyEvent.VK_DOWN);
+		r.keyRelease(KeyEvent.VK_DOWN);
+	}
+}
